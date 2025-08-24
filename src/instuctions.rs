34cd241f3337335eeca366348194dcbb50bb143c
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-24 23:20:19 +0530

    initialised instructions

---
+++ src/instuctions.rs
@@ -0,0 +1,13 @@
+use solana_program::pubkey::Pubkey;
+
+pun enum EscrowInstruction {
+    InitEscrow { //token type and how much
+        token_A_mint: Pubkey,
+        token_B_mint: Pubkey,
+        amount_A: u64,
+        amount_b: u64,
+    },
+    Deposit,
+    CompleteSwap,
+    Cancel,
+}
