f34a1617629817ff7018e795be3f28acf32cfe52
saatvik-10 <saatvikmadan529@gmail.com>
2025-08-26 01:35:50 +0530

    added borsh

--- src/processor.rs
+++ src/processor.rs
@@ -1,16 +1,27 @@
+use crate::errors::EscrowError;
+use crate::instructions::{EscrowInstruction, check_rent_exempt};
+use borsh::BorshDeserialize;
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
-
-use crate::instruction::EscrowInstruction;
 
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let instruction = EscrowInstruction::unpack(instruction_data)?;
+    let instruction = EscrowInstruction::try_from_slice(instruction_data)?;
 
     match instruction {
-        EscrowInstruction::InitEscrow { .. } => {}
+        EscrowInstruction::InitEscrow { .. } => {
+            let user_a_account = &accounts[0];
+            let escrow_account = &accounts[1];
+            let rent_account = &accounts[2];
+
+            if !user_a_account.is_signer {
+                return Err(EscrowError::UnauthorizedCancel.into());
+            }
+
+            check_rent_exempt(escrow_account, rent_account)?;
+        }
 
         EscrowInstruction::Deposit { .. } => {}
 
@@ -19,5 +30,5 @@ pub fn process_instruction(
         EscrowInstruction::Cancel { .. } => {}
     }
 
-    Ok(());
+    Ok(())
 }
