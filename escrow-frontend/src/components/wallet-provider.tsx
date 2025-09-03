af249971f872a159b94d8bb4d7228cbcaa6fbf9f
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-03 20:05:51 +0530

    added wallet provider

---
+++ escrow-frontend/src/components/wallet-provider.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+interface WalletProviderProps {
+  children: React.ReactNode;
+}
+
+export const WalletContextProvider: React.FC<WalletProviderProps> = ({
+  children,
+}) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = clusterApiUrl(network);
+
+  const wallets = React.useMemo(() => [], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
