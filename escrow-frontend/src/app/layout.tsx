345fc3bac9dac5ff2dce93162858e70d4fb31cfa
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-05 20:58:08 +0530

    added theme provider

--- escrow-frontend/src/app/layout.tsx
+++ escrow-frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { WalletContextProvider } from '@/components/wallet-provider';
 import { Toaster } from 'react-hot-toast';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,8 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <WalletContextProvider>{children}</WalletContextProvider>
-        <Toaster />
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <WalletContextProvider>{children}</WalletContextProvider>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
